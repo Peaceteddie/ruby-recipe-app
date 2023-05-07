83a74ad8fbbb211b1314434c34a0d48f05e7d5a2
tithxa <tithxa@syntronic.com>
2023-05-07 03:35:07 +0200

    Fixed front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get "/recipes", to: "recipes#index"
-  get '/recipes/:slug', to: 'recipes#show_by_slug', as: 'recipe_slug'
+
+  root "recipes#index"
+
+
+
+  get "recipes/:slug", to: "recipes#show_by_slug", as: "recipe_slug"
+
 end
+
